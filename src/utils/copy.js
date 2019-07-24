6d3ddb337b774d31376cd47e3ba7af83b80c1056
mehmet-erim <bilgi@mehmeterim.com>
2019-07-24 23:30:17 +0300

    feature: develop copy util, add prettierrc

--- src/utils/copy.js
+++ src/utils/copy.js
@@ -1,8 +1,32 @@
 import fse from 'fs-extra';
+import { Log } from './log';
 
 export default async function(outputFolderPath) {
   const packageJson = await fse.readJSON(`${outputFolderPath}/package.json`);
-  console.log(packageJson);
+  const { name } = packageJson;
+  const hasScope = name.indexOf('@') > -1;
+
+  let nodeModulePath = name;
+  if (hasScope) {
+    const nameObj = parseScopedName(name);
+    nodeModulePath = `${nameObj.scope}/${nameObj.name}`;
+  }
+
+  if (fse.existsSync(`node_modules/${nodeModulePath}`)) {
+    await fse.remove(`node_modules/${nodeModulePath}`);
+  }
+
+  try {
+    await fse.copy(outputFolderPath, `node_modules/${nodeModulePath}`, {});
+  } catch (error) {
+    Log.error(`An error occured. While copying process. Error: ${error}`);
+  }
 }
 
-function getScope(packageName) {}
+function parseScopedName(packageName) {
+  const splitted = packageName.split('/');
+  return {
+    scope: splitted[0],
+    name: splitted[1],
+  };
+}
